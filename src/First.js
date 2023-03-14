8c6fef4bd51b0a58bf173b605992615ea9f8f0e8
Prajwal-audacious <prajwal.ramanwal@techiegent.com>
2023-03-14 14:57:27 +0530

    Feat:firstCommit

---
+++ src/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Second,Third} from "./Multi"
+const First = () => {
+  return (
+    <div>
+      <h1>this is Our first comonent1</h1>
+    <Second/>
+    <Third/>
+    
+    </div>
+  )
+}
+
+export default First
---
+++ src/Multi.js
@@ -0,0 +1,16 @@
+
+export const Second=()=>{
+      return (
+            <>
+            <h1>This is our multi Second</h1>
+            </>
+      )
+}
+
+export const Third=()=>{
+      return(
+      <> 
+      <h1>This is our multi Third</h1>
+      </>
+      )
+}
